b08c9c59dd356b1ccb8eb337dde5bf53d4c741e9
PolovkaOleksandr <120940683+PolovkaOleksandr@users.noreply.github.com>
2026-02-02 18:19:19 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string message = Console.ReadLine();
+        if (message.StartsWith("H3i9AH")) {
+            message = message.Substring(6);
+            string[] code = message.Split(new string[] {"weather "}, StringSplitOptions.None);
+            foreach (string part in code)
+            {
+                Console.WriteLine(part);
+            }
+            
+        }
+        else if (message == null)
+        {
+            Console.WriteLine("No input provided.");
+            return;
+        }
+        else{
+            Console.WriteLine("ok");
+            }
+         // True
+    }
+}
